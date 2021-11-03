ff18e1bf074b019effef0bbaaa03235e73891303
skedwards88 <skedwards88@github.com>
2021-11-03 09:51:49 -0700

    convert to hooks, update styling, code improvements

---
+++ src/App.js
@@ -0,0 +1,402 @@
+import React from "react";
+import "./App.css";
+import { polyfill } from "mobile-drag-drop";
+
+import calculateScore from "./scoring";
+import {
+  getSurroundingIndexes,
+  noChoicesRemaining,
+  autoFillRemaining,
+} from "./helpers.js";
+import { rules } from "./rules.js";
+
+polyfill({
+  dragImageCenterOnTouch: true,
+});
+
+// todo
+// make sure works on various width/orientation
+// add tests for getSurroundingIndexes and other helpers
+// new favicon
+// local storage
+// linters
+
+function Board({ squares, playerColor, showHints, hintShape, dropToken }) {
+  const board = squares.map((square, index) => {
+    let classes = ["square"];
+
+    if (square.symbol) {
+      classes.push(square.symbol);
+    }
+
+    if (square.color) {
+      classes.push(square.color);
+    } else if (
+      !square.valid.red.star &&
+      !square.valid.red.circle &&
+      !square.valid.blue.star &&
+      !square.valid.blue.circle
+    ) {
+      classes.push("black");
+    } else if (showHints && hintShape && square.valid[playerColor][hintShape]) {
+      classes.push(`${playerColor}Hint`);
+    }
+
+    const className = classes.join(" ");
+
+    return (
+      <div
+        key={index}
+        className={className}
+        onDragOver={(event) => {
+          event.preventDefault();
+        }}
+        onDrop={(event) => dropToken({ event: event, index: index })}
+        onDragEnter={(event) => {
+          event.preventDefault();
+        }}
+      ></div>
+    );
+  });
+  return <div className="board">{board}</div>;
+}
+
+function Game({ playHistory, setPlayHistory, showHints }) {
+  console.log("GAME");
+  const [hintShape, setHintShape] = React.useState(null);
+
+  const gridSize = Math.sqrt(playHistory[0].length);
+  const squares = [...playHistory[playHistory.length - 1]];
+  const turnNumber = playHistory.length;
+  const playerColor = turnNumber % 2 ? "red" : "blue";
+  const opponentColor = turnNumber % 2 ? "blue" : "red";
+
+  function dragToken({ event, symbol }) {
+    event.dataTransfer.setData("symbol", symbol);
+    // If not on a device on which the mobile-drag-drop pollyfill applies
+    // Center the drag image on the cursor
+    if (!/iPad|iPhone|iPod|Android/.test(navigator.userAgent)) {
+      ev.dataTransfer.setDragImage(ev.target, 50, 50);
+    }
+  }
+
+  function dropToken({ event, index }) {
+    // todo does calling this from a different component break a hook rule?
+    const symbol = event.dataTransfer.getData("symbol");
+
+    if (showHints) {
+      // todo does this break hook rule
+      setHintShape(null);
+    }
+
+    // If the turn is illegal, return early //todo does that break a hook rule? yes? should put conditional within hook
+    if (!squares[index].valid[playerColor][symbol]) {
+      console.log("invalid");
+      return;
+    }
+
+    let squaresCopy = JSON.parse(
+      JSON.stringify(playHistory[playHistory.length - 1])
+    ); // todo how to make deep copy?
+    squaresCopy[index].symbol = symbol;
+    squaresCopy[index].color = playerColor;
+
+    // Placement in the current index is no longer valid
+    squaresCopy[index].valid.red.star = false;
+    squaresCopy[index].valid.blue.star = false;
+    squaresCopy[index].valid.red.circle = false;
+    squaresCopy[index].valid.blue.circle = false;
+
+    // Placement in the surrounding tiles is now invalid for the same shape of the opposite color
+    const surroundingIndexes = getSurroundingIndexes({
+      index: index,
+      gridSize: gridSize,
+    });
+    surroundingIndexes.map((surroundingIndex) => {
+      squaresCopy[surroundingIndex].valid[opponentColor][symbol] = false;
+    });
+
+    if (noChoicesRemaining(squaresCopy)) {
+      squaresCopy = autoFillRemaining(squaresCopy);
+    }
+
+    setPlayHistory([...playHistory, squaresCopy]);
+  }
+
+  function handleMouseDown(symbol) {
+    console.log("MOUSE DOWN");
+    if (showHints) {
+      setHintShape(symbol);
+    }
+  }
+
+  function handleMouseUp() {
+    if (showHints) {
+      setHintShape(null);
+    }
+  }
+
+  console.log("RENDER");
+  return (
+    <div className="game">
+      <Board
+        squares={squares}
+        playerColor={playerColor}
+        hintShape={hintShape}
+        showHints={showHints}
+        dropToken={dropToken}
+      />
+      <div id="tokens">
+        <div
+          draggable="true"
+          onDragStart={(event) => dragToken({ event: event, symbol: "star" })}
+          className={`square star ${playerColor}`}
+          onMouseDown={() => handleMouseDown("star")}
+          onTouchStart={() => handleMouseDown("star")}
+          onMouseUp={handleMouseUp}
+        />
+        <div
+          draggable="true"
+          onDragStart={(event) => dragToken({ event: event, symbol: "circle" })}
+          className={`square circle ${playerColor}`}
+          onMouseDown={() => handleMouseDown("circle")}
+          onTouchStart={() => handleMouseDown("circle")}
+          onMouseUp={handleMouseUp}
+        />
+      </div>
+    </div>
+  );
+}
+
+function NewGameSettings({
+  playHistory,
+  setPlayHistory,
+  showHints,
+  setShowHints,
+}) {
+  const [showNewGameSettings, setShowNewGameSettings] = React.useState(false);
+  const currentGridSize = Math.sqrt(playHistory[0].length);
+  const [selectedGridSize, setSelectedGridSize] =
+    React.useState(currentGridSize);
+
+  function handleNewGame(event) {
+    event.preventDefault();
+    const newGridSize = event.target.elements.gridSize.value;
+    setSelectedGridSize(newGridSize);
+    setPlayHistory([
+      Array.from({ length: newGridSize * newGridSize }, () => ({
+        color: "",
+        symbol: "",
+        valid: {
+          red: {
+            star: true,
+            circle: true,
+          },
+          blue: {
+            star: true,
+            circle: true,
+          },
+        },
+      })),
+    ]);
+    setShowNewGameSettings(false);
+  }
+
+  // This component function can return either the form or the "new game" button
+  // Since the useEffect callback will only run when selectedGridSize changes,
+  // and since the form closes (and we instead render the "new game" button) after changing selectedGridSize
+  // `ref={ref}` is on the the "new game" button instead of on the form
+  const ref = React.useRef();
+  React.useEffect(() => {
+    ref.current.parentElement.parentElement.style.setProperty(
+      "--num_columns",
+      `${selectedGridSize}`
+    );
+
+    // todo I'm not sure if useRef is better than document.getElementById when you want a parent
+    //    Creating the ref in the parent fails, but forwardRef might be a solution
+    // const node = document.getElementById("App");
+    // node.style.setProperty("--num_columns", `${selectedGridSize}`);
+  }, [selectedGridSize]);
+
+  if (showNewGameSettings) {
+    return (
+      <form className="modal" onSubmit={handleNewGame}>
+        <div id="settings">
+          <div className="setting">
+            <div className="settingDescription">
+              <label htmlFor="gridSize">Grid size</label>
+            </div>
+            <select id="gridSize" defaultValue={currentGridSize}>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+            </select>
+          </div>
+
+          <div className="setting">
+            <div className="settingDescription">
+              <label htmlFor="hints">Hints </label>
+              <div className="settingInfo">Highlight valid moves</div>
+            </div>
+
+            <input
+              name="hints"
+              type="checkbox"
+              checked={showHints}
+              onChange={() => setShowHints(!showHints)}
+            />
+          </div>
+
+          <div className="setting">
+            <div className="settingDescription">
+              <label htmlFor="islands">Islands</label>
+              <div className="settingInfo">
+                Start the game with blacked out spaces (coming soon)
+              </div>
+            </div>
+            <select name="islands"></select>
+          </div>
+
+          <div className="setting">
+            <div className="settingDescription">
+              <label htmlFor="bombs">Bombs</label>
+              <div className="settingInfo">
+                Give each player a single-use bomb to clear one spot on the
+                board (coming soon)
+              </div>
+            </div>
+            <input name="bombs" type="checkbox" disabled />
+          </div>
+        </div>
+        <div id="newGameButtons">
+          <button type="submit">New Game</button>
+          <button type="button" onClick={() => setShowNewGameSettings(false)}>
+            Cancel
+          </button>
+        </div>
+      </form>
+    );
+  } else {
+    return (
+      <button
+        ref={ref}
+        id="newGame"
+        onClick={() => setShowNewGameSettings(true)}
+      ></button>
+    );
+  }
+}
+
+function Score({ playHistory }) {
+  const squares = [...playHistory[playHistory.length - 1]];
+  const gridSize = Math.sqrt(squares.length);
+
+  const score = calculateScore({ squares: [...squares], gridSize: gridSize });
+  const redScore = score.red;
+  const blueScore = score.blue;
+
+  return (
+    <div id="score">
+      <div id="redScore">{`Red: ${redScore}`}</div>
+      <div id="blueScore">{`Blue: ${blueScore}`}</div>
+    </div>
+  );
+}
+
+function Undo({ playHistory, setPlayHistory }) {
+  function handleUndo() {
+    if (playHistory.length > 0) {
+      setPlayHistory(playHistory.slice(0, playHistory.length - 1));
+    }
+  }
+
+  return (
+    <button
+      id="undo"
+      onClick={handleUndo}
+      disabled={playHistory.length === 1}
+    />
+  );
+}
+
+function Rules() {
+  const [showRules, setShowRules] = React.useState(true);
+  const [currentRule, setCurrentRule] = React.useState(2);
+
+  function closeRules() {
+    setShowRules(false);
+    setCurrentRule(0);
+  }
+  
+  if (showRules) {
+    return (
+      <div className="modal" id="rulesModal">
+        {rules[currentRule]}
+        <div id="ruleNavigation">
+          <button
+            disabled={!currentRule}
+            onClick={() => setCurrentRule(currentRule - 1)}
+          >
+            Previous
+          </button>
+          <button onClick={closeRules}>Close</button>
+          <button
+            disabled={currentRule === rules.length - 1}
+            onClick={() => setCurrentRule(currentRule + 1)}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <button id="rulesButton" onClick={() => setShowRules(true)}></button>
+    );
+  }
+}
+
+function App() {
+  const defaultGridSize = 5;
+  const [playHistory, setPlayHistory] = React.useState([
+    Array.from({ length: defaultGridSize * defaultGridSize }, () => ({
+      color: "",
+      symbol: "",
+      valid: {
+        red: {
+          star: true,
+          circle: true,
+        },
+        blue: {
+          star: true,
+          circle: true,
+        },
+      },
+    })),
+  ]);
+  const [showHints, setShowHints] = React.useState(true);
+
+  return (
+    <div id="App">
+      <Game
+        playHistory={playHistory}
+        setPlayHistory={setPlayHistory}
+        showHints={showHints}
+      />
+      <Score playHistory={playHistory} />
+      <div id="controls">
+        <Undo playHistory={playHistory} setPlayHistory={setPlayHistory} />
+        <NewGameSettings
+          playHistory={playHistory}
+          setPlayHistory={setPlayHistory}
+          showHints={showHints}
+          setShowHints={setShowHints}
+        />
+        <Rules />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/helpers.js
@@ -0,0 +1,68 @@
+function getSurroundingIndexes({ index, gridSize }) {
+  const column = index % gridSize;
+  const row = Math.floor(index / gridSize);
+  let surroundingIndexes = [];
+  for (let currentRow = row - 1; currentRow <= row + 1; currentRow++) {
+    for (
+      let currentColumn = column - 1;
+      currentColumn <= column + 1;
+      currentColumn++
+    ) {
+      if (
+        currentRow >= 0 &&
+        currentColumn >= 0 &&
+        currentRow < gridSize &&
+        currentColumn < gridSize
+      ) {
+        const currentIndex = currentColumn + currentRow * gridSize;
+        surroundingIndexes.push(currentIndex);
+      }
+    }
+  }
+  return surroundingIndexes;
+}
+
+function noChoicesRemaining(squares) {
+  // Return true if BOTH red and blue have valid moves
+  const validRedMoves = squares.filter(
+    (square) => square.valid.red.circle || square.valid.red.star
+  );
+  const validBlueMoves = squares.filter(
+    (square) => square.valid.blue.circle || square.valid.blue.star
+  );
+  if (validRedMoves.length && validBlueMoves.length) {
+    console.log("NOT END GAME");
+    return false;
+  } else {
+    console.log("IS END GAME");
+    return true;
+  }
+}
+
+function autoFillRemaining(squares) {
+  // Should only call when either red or blue have no more valid moves; otherwise, squares will be filled with a bias
+
+  // If all squares are already filled, return
+  if (!squares.filter(i=>!i.color).length) {
+    return squares
+  }
+
+  console.log("AUTO FILL");
+  alert(
+    "No choices left for remaining squares! Empty squares will be filled automatically."
+  ); // todo make alert nicer looking, popup instead?
+  squares.map((square) => {
+    if (!square.color) {
+      if (square.valid.red.circle || square.valid.red.star) {
+        square.color = "red";
+      }
+      if (square.valid.blue.circle || square.valid.blue.star) {
+        square.color = "blue";
+      }
+    }
+  });
+
+  return squares;
+}
+
+export {getSurroundingIndexes, noChoicesRemaining, autoFillRemaining}
